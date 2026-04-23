2050350cec56b18e26c313564feffb01e56ffd56
Harry Munro <harry.munro@thevalueengineers.com>
2026-04-23 10:44:16 +0100

    Add new targets

--- _targets.R
+++ _targets.R
@@ -1,29 +1,42 @@
+rm(list=ls());gc()
+
 library(targets)
 library(tarchetypes)
 library(tveDataLoader)
 library(tveDevTools)
 library(tveTableTool)
+library(renv)
+library(dplyr)
+library(stringr)
 
+#renv::init()
+#renv::restore()
 
-# Run the R scripts in the R/ folder with your custom functions:
+# Run the R scripts in the R/ 
 targets::tar_source()
 
 # Run the R script containing all constant values used in pipeline
 targets::tar_source(here::here('inst', 'scripts', 'objects.R'))
 
-
-# End this file with a list of target objects.
+silly_test <- list(
+  tar_target(s_test_5, test5()),
+  tar_target(s_test_10, test10()),
+  tar_target(s_test_20, s_test_10*2),
+  tar_target(s_output, s_test_5-(sum(s_test_20)))
+)
+# List of target objects.
 results <- list(
   tar_target(data, 
-    load_sav(data_path = here::here("data",
+    tveDataLoader::load_sav(data_path = here::here("data",
     "5216196 - The Value Engineers, Target audience & growth strategy - Total File.sav"
   ),
   tibble_out = F
 )),
-  tar_target(test_5, test5()),
-  tar_target(test_10, test10()),
-  tar_target(test_20, test_10*2),
-  tar_target(output, test_5-(sum(test_20))) # Call your custom functions.
+  silly_test,
+  targets::tar_target(data_vars,data$var_labels),
+  targets::tar_target(var_scramble, 
+    data_vars %>%
+      mutate(vars_scramble = (as.numeric(str_remove_all(var_name,"\\D"))+sum(s_test_10))))
 )
 
-
+#renv::snapshot()
